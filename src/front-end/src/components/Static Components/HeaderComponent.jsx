b4ad928a32dc819f01c4e91dc2c001674bcba4d1
Mefius <mateuscpacifico04@gmail.com>
2024-04-11 14:41:58 -0300

    Nothing important

--- src/front-end/src/components/Static Components/HeaderComponent.jsx
+++ src/front-end/src/components/Static Components/HeaderComponent.jsx
@@ -6,7 +6,7 @@ const HeaderComponent = () => {
       <header className='header-fixed'>
         <nav className='navbar navbar-dark bg-dark'>
           <div className='header-text'>
-            <a className="navbar-brand" href="http://localhost:1000/simple-characters-list">Character Manegement System</a>
+            <a className="navbar-brand" href="http://localhost:1000/simple-characters-list">Character Management System</a>
           </div>
         </nav>
       </header>
